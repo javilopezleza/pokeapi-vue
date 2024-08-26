127cec662ff4d26b702daa1c27911ded199e28b5
javilopezleza <javilopezleza90@gmail.com>
2024-08-26 19:59:10 +0200

    POKEAPI VueJs

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/pokemonCard',
+      path: '/',
       name: 'pokemonCard',
 
       component: PokemonListView
